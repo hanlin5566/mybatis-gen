3a7408fa38f6765fa2c2a3fa291484d5c19558c7
huhl <huhl@gitlab.hzbd.com>
2017-11-14 17:25:26 +0800

    Initial commit

---
+++ src/main/java/com/hzcf/mybatis/format/MyFormatter.java
@@ -0,0 +1,32 @@
+package com.hzcf.mybatis.format;
+
+import org.mybatis.generator.api.JavaFormatter;
+import org.mybatis.generator.api.dom.DefaultJavaFormatter;
+import org.mybatis.generator.api.dom.java.CompilationUnit;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.config.Context;
+
+/**
+ * Create by hanlin on 2017年11月6日
+ **/
+
+public class MyFormatter extends DefaultJavaFormatter implements JavaFormatter {
+
+    @Override
+    public String getFormattedContent(CompilationUnit unit) {
+
+        FullyQualifiedJavaType type = unit.getType();
+
+        String name = type.getFullyQualifiedName();
+
+        System.out.println(name);
+        return unit.getFormattedContent();
+    }
+
+    @Override
+    public void setContext(Context arg0) {
+
+    }
+
+}
+
